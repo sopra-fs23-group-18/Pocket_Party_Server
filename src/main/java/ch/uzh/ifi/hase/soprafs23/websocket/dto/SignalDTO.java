57cb0264b740d0580d1295823ad4e82c16164445
ngrob <nils.grob@protonmail.com>
2023-03-28 10:31:01 +0200

    webRTC wip

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/websocket/dto/SignalDTO.java
@@ -0,0 +1,41 @@
+package ch.uzh.ifi.hase.soprafs23.websocket.dto;
+
+import ch.uzh.ifi.hase.soprafs23.constant.SignalType;
+
+public class SignalDTO {
+    private String data, senderId, recipentId;
+    private SignalType type;
+
+    public SignalType getType() {
+        return type;
+    }
+
+    public void setType(SignalType type) {
+        this.type = type;
+    }
+
+    public String getRecipentId() {
+        return recipentId;
+    }
+
+    public void setRecipentId(String recipentId) {
+        this.recipentId = recipentId;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
