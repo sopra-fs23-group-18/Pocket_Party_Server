db78509e85932f504739775930a3d7300b7a66ef
steschu <stefan.schuler@uzh.ch>
2023-03-29 17:14:19 +0200

    #34 added setter and getters for minigame

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Minigame.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Minigame.java
@@ -13,9 +13,48 @@ public abstract class Minigame {
         this.scoreToGain = scoreToGain;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeam2Player() {
+        return team2Player;
+    }
+
+    public void setTeam2Player(String team2Player) {
+        this.team2Player = team2Player;
+    }
+
+    public String getTeam1Player() {
+        return team1Player;
+    }
+
+    public void setTeam1Player(String team1Player) {
+        this.team1Player = team1Player;
+    }
+
+    public int getScoreToGain() {
+        return scoreToGain;
+    }
+
     public void setWinner(Team winner) {
         this.winner = winner;
         isFinished = true;
     }
+
+    public Team getWinner() {
+        return winner;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+
+    
 
 }
