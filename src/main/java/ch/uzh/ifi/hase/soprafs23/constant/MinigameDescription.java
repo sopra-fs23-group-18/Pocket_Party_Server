6b8e7475dae3dda262600a13f76452dab012e60a
steschu <stefan.schuler@uzh.ch>
2023-05-03 17:10:05 +0200

    updated description & type to include potato game

--- src/main/java/ch/uzh/ifi/hase/soprafs23/constant/MinigameDescription.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/constant/MinigameDescription.java
@@ -7,7 +7,8 @@ public final class MinigameDescription {
 
     private static EnumMap<MinigameType, String> minigames = new EnumMap<>(Map.ofEntries(
         Map.entry(MinigameType.TAPPING_GAME, "Tap the screen as fast as you can!"), 
-        Map.entry(MinigameType.TIMING_GAME, "Shake your phone at the right time to catch the objects falling from the sky!")
+        Map.entry(MinigameType.TIMING_GAME, "Shake your phone at the right time to catch the objects falling from the sky!"),
+        Map.entry(MinigameType.HOT_POTATO, "The players toss a potato to each other while time is counting down. The player who is holding the object when the timer reaches 0 is eliminated.")
         ));
     
     private MinigameDescription(){
