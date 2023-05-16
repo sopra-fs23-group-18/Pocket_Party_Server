7cfbd0099cf880d86457ba3e819d6e1b3d632578
steschu <stefan.schuler@uzh.ch>
2023-05-16 14:26:37 +0200

    added description for newly added types

--- src/main/java/ch/uzh/ifi/hase/soprafs23/constant/MinigameDescription.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/constant/MinigameDescription.java
@@ -9,7 +9,9 @@ public final class MinigameDescription {
         Map.entry(MinigameType.TAPPING_GAME, "Tap the screen as fast as you can!"), 
         Map.entry(MinigameType.TIMING_GAME, "Shake your phone at the right time to catch the objects falling from the sky!"),
         Map.entry(MinigameType.HOT_POTATO, "The players toss a potato to each other while time is counting down. The player who is holding the object when the timer reaches 0 is eliminated."),
-        Map.entry(MinigameType.VIBRATION_GAME, "Fix")
+        Map.entry(MinigameType.VIBRATION_GAME, "Fix"),
+        Map.entry(MinigameType.PONG_GAME, "Fix"),
+        Map.entry(MinigameType.RPS_GAME, "Fix")
         ));
     
     private MinigameDescription(){
