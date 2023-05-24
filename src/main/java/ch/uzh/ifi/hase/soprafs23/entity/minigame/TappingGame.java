a7ae2090f7b8c230c0e153142832fa751fac805c
Byron Wu <wuguoaiw@gmail.com>
2023-05-24 12:33:17 +0200

    Description for Pong, RPS, and Strategy

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/minigame/TappingGame.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/minigame/TappingGame.java
@@ -1,34 +1,18 @@
 package ch.uzh.ifi.hase.soprafs23.entity.minigame;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-
-import javax.persistence.InheritanceType;
-import javax.persistence.ManyToMany;
-
 import ch.uzh.ifi.hase.soprafs23.constant.MinigamePlayers;
 import ch.uzh.ifi.hase.soprafs23.constant.MinigameType;
 
 @Entity
-public class TappingGame extends Minigame{
+public class TappingGame extends Minigame {
 
     public TappingGame() {
         super(
-            MinigameType.TAPPING_GAME, 
-            "Tap the screen as fast as you can!", 
-            500,
-            new MinigamePlayers[] {MinigamePlayers.ONE}
-        );
+                MinigameType.TAPPING_GAME,
+                "Tap the screen as fast as you can!",
+                500,
+                new MinigamePlayers[] { MinigamePlayers.ONE });
     }
-
-    
 
 }
